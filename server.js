98e501f4f20786dd083d544d8e75e2a00d709d67
cloudwithandy <andygarciacloud@gmail.com>
2026-03-25 16:52:03 -0400

    Fix startup by serving index.html with node

---
+++ server.js
@@ -0,0 +1,24 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+const port = process.env.PORT || 8080;
+
+const server = http.createServer((req, res) => {
+const filePath = path.join(__dirname, "index.html");
+
+fs.readFile(filePath, (err, content) => {
+if (err) {
+res.writeHead(500, { "Content-Type": "text/plain" });
+res.end("Server error");
+return;
+}
+
+res.writeHead(200, { "Content-Type": "text/html" });
+res.end(content);
+});
+});
+
+server.listen(port, () => {
+console.log(`Server running on port ${port}`);
+});
